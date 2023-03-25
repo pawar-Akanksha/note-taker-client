2487288e98f680720f1d10a0549f8ece0c5b3b03
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-25 03:21:07 -0700

    Added changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import './Navbar.css';
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
+import { useEffect } from "react";
 
 const NavBar = () => {
+    const {userInfo,setUserInfo} = useContext(UserContext);
+    useEffect(()=>{
+        fetch("",{
+            credentials:"include",
+        }).then((response)=>{
+            response.json().then((userInfo)=>{
+                setUserInfo(userInfo)
+            });
+        })
+    },[]);
+
+    function logout(){
+        fetch("http://localhost:4000/api/logout",{
+            credentials:"include",
+            method:"POST",
+        });
+        setUserInfo(null);
+        console.log("USER LOGOUT")
+    }
+    const email = userInfo?.email;
+   
     return(
         <div className="navcont">
             <nav>
@@ -19,6 +43,11 @@ const NavBar = () => {
                 <li>
                     <Link id="l3" to={"/deleteAll"}>
                         DeleteAll
+                    </Link>
+                </li>
+                <li>
+                    <Link id="14" onClick={logout} to={"/"}>
+                        {email} Logout
                     </Link>
                 </li>
             </nav>
