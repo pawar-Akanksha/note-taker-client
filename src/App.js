ef0f9679db7d893254dfe47cfc0da87b4e404c5a
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-25 03:24:10 -0700

    added changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import UserContextProvider from "./UserContext";
 
 function App() {
   return <Router>
+    <UserContextProvider>
     <Routes>
     <Route index path="/" element={<Login/>} />
     <Route path="/signup" element={<SignUp/>} />
     <Route path="/home" element={<Home/>} />
     </Routes>
+    
+    </UserContextProvider>
   </Router>;
 }
 
